3bf76ee3741f15c31d4d923a9b1bd39d2ae4e27b
svidunovichromanv <svidunovichromanv@gmail.com>
2018-10-18 21:01:48 +0300

    mobile edit and delete

---
+++ Mobile/components/MobileClient.js
@@ -0,0 +1,89 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './MobileClient.css';
+
+class MobileClient extends React.PureComponent {
+
+  static propTypes = {
+    cbDelete: PropTypes.func.isRequired,
+    info:PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      fio: PropTypes.string.isRequired,
+      balance: PropTypes.number.isRequired,
+    }),
+  };
+
+  state = {
+    info: this.props.info,
+      editState:false
+  };
+
+  componentWillReceiveProps = (newProps) => {
+    console.log("MobileClient id="+this.props.info.id+" componentWillReceiveProps");
+    this.setState({info:newProps.info});
+  };
+
+  deleteCl=()=>{
+    this.props.cbDelete(this.props.info.id);
+  };
+
+  editCl = () =>{
+    if (this.state.editState && (this.balance!==this.state.info.balance || this.fio!==this.state.info.fio)){
+      let info = {...this.state.info, fio:this.fio, balance:this.balance};
+      this.setState({info:info});
+    }else{
+        this.balance=this.state.info.balance;
+        this.fio=this.state.info.fio;
+    }
+      this.setState( (prevState) => { return {editState:!prevState.editState}; } );//рендер при любом нажатии, так и должно быть, вроде....
+  };
+
+    recordBalance=(e)=>{
+        this.balance = e.target.value;
+    };
+
+    recordFIO=(e)=>{
+      this.fio= e.target.value;
+    };
+
+  render() {
+
+    console.log("MobileClient id="+this.state.info.id+" render");
+
+    let data;
+    if (!this.state.editState){
+      data = (
+          <div className='MobileClient'>
+        <button className="MobileClientBtn" onClick={this.deleteCl}>
+          del
+        </button>
+        <button className="MobileClientBtn" onClick={this.editCl}>
+          edit
+        </button>
+        <span className='MobileClientBalance'>{this.state.info.balance}</span>
+        <span className='MobileClientFIO'>{this.state.info.fio}</span>
+        </div>
+      );
+    }else{
+        data = (
+            <div className='MobileClient'>
+              <button className="MobileClientBtn" onClick={this.deleteCl}>
+                del
+              </button>
+              <button className="MobileClientBtn" onClick={this.editCl}>
+                edit
+              </button>
+              <input defaultValue={this.state.info.balance} onChange={this.recordBalance}/>
+              <input defaultValue={this.state.info.fio} onChange={this.recordFIO}/>
+            </div>
+        );
+    }
+
+    return data;
+
+  }
+
+}
+
+export default MobileClient;
