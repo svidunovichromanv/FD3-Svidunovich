07091f84ed9f4f2f2ad02a73fb73bf5bb4eaffe3
svidunovichromanv <svidunovichromanv@gmail.com>
2018-11-07 16:38:55 +0300

    Scales3

--- Scales3/app.ts
+++ Scales3/app.ts
@@ -1,85 +1,99 @@
-
-class Scales {
-
-    private products:Array<IProduct>;
-
+interface IStorageEngine{
+    addItem(item:Product):void;
+    getItem(index:number):Product;
+    getCount():number;
+}
+class Product{
+    private name:string;
+    private scale:number;
+    constructor(name:string, scale:number) {
+        this.name=name;
+        this.scale=scale;
+    }
+    getName():string {
+        return " "+this.name;
+    }
+    getScale():number {
+        return this.scale;
+    }
+}
+class ScalesStorageEngineArray implements IStorageEngine {
+    storage:Array<Product>;
     constructor() {
-        this.products=[];
+        this.storage=[];
+    }
+    addItem(item:Product):void {
+        this.storage.push(item);
+    }
+    getItem(index:number):Product {
+        return this.storage[index];
+    }
+    getCount():number{
+        return this.storage.length;
+    }
+}
+class ScalesStorageEngineLocalStorage implements IStorageEngine {
+    key:string;
+    tempStorege:Array<Product>;
+    constructor(key:string) {
+        this.key=key;
+        this.tempStorege=JSON.parse(localStorage.getItem(this.key));
+    }
+    addItem(item:Product):void {
+        let stringItem:string;
+        this.tempStorege.push(item);
+        stringItem=JSON.stringify(this.tempStorege);
+        localStorage.setItem(this.key, stringItem);
+    }
+    getItem(index:number):Product {
+        return this.tempStorege[index];
+    }
+    getCount():number{
+        return this.tempStorege.length;
+    }
+}
+class Scales <StorageEngine extends IStorageEngine>{
+
+    private storage:StorageEngine;
+
+    constructor(storage:StorageEngine) {
+        this.storage=storage;
     }
 
-    addProduct(p:IProduct):void {
-        this.products.push(p);
+    addProduct(p:Product):void {
+        this.storage.addItem(p);
     }
-    getSumScale():void{
-        let summ:number=0;
-        for(let i:number=0; i<this.products.length; i++){
-            summ+=this.products[i].getScale();
+    getSumScale():number{
+        let sum:number=0;
+        let quantity:number=this.storage.getCount();
+        for(let i:number=0; i<quantity; i++){
+            sum+=this.storage.getItem(i).getScale();
         }
-        console.log(summ);
+        return sum;
     }
-    getNameList():void{
+    getNameList():string{
         let list:string="";
-        for(let i:number=0; i<this.products.length; i++){
-            list+=this.products[i].getName();
+        let quantity:number=this.storage.getCount();
+        for(let i:number=0; i<quantity; i++){
+            list+=this.storage.getItem(i).getName();
         }
-        console.log(list);
+        return list;
     }
 }
 
-interface IProduct{
-    getName():string;
-    getScale():number;
-}
-
-class Apple implements IProduct {
-    private name:string;
-    private scale:number;
-    constructor(name:string, scale:number) {
-        this.name=name;
-        this.scale=scale;
-    }
-    getName():string {
-        return " "+this.name;
-    }
-    getScale():number {
-        return this.scale;
-    }
-}
-class Tomato implements IProduct {
-    private name:string;
-    private scale:number;
-    constructor(name:string, scale:number) {
-        this.name=name;
-        this.scale=scale;
-    }
-    getName():string {
-        return " "+this.name;
-    }
-    getScale():number {
-        return this.scale;
-    }
-}
-const scale1:Scales=new Scales();
-const apple1:Apple=new Apple("cashtelia5", 5);
-const apple2:Apple=new Apple("malinka1", 1);
-const tomat1:Tomato=new Tomato("cheri2", 2);
-const tomat2:Tomato=new Tomato("orange4", 4);
-scale1.addProduct(apple1);
-scale1.getNameList();
-scale1.getSumScale();
-
-scale1.addProduct(apple2);
-scale1.getNameList();
-scale1.getSumScale();
-
-scale1.addProduct(tomat1);
-scale1.getNameList();
-scale1.getSumScale();
-
-scale1.addProduct(tomat2);
-scale1.getNameList();
-scale1.getSumScale();
-
-scale1.addProduct(apple1);
-scale1.getNameList();
-scale1.getSumScale();
+const apple1:Product= new Product("apple1",1);
+const apple2:Product= new Product("apple2",2);
+const tomato1:Product= new Product("tomato1",1);
+const tomato2:Product= new Product("tomato2",2);
+const storageArray=new ScalesStorageEngineArray();
+const scalesArray=new Scales <ScalesStorageEngineArray>(storageArray);
+const storageLocalStorage=new ScalesStorageEngineLocalStorage("Products");
+const scalesLocalStorage=new Scales <ScalesStorageEngineLocalStorage>(storageLocalStorage);
+scalesArray.addProduct(apple1);
+scalesArray.addProduct(tomato1);
+scalesLocalStorage.addProduct(apple2);
+scalesLocalStorage.addProduct(tomato2);
+console.log("names from array: "+scalesArray.getNameList());
+console.log("scale from array: "+scalesArray.getSumScale());
+console.log("names from LH: "+scalesLocalStorage.getNameList());
+console.log("scale from LH: "+scalesLocalStorage.getSumScale());
